a5922592e3bd865b6c9c7a03fa546ccad1acba8c
LiviaVilarinho <liviavilarinho@yahoo.com.br>
2022-07-10 21:27:32 -0300

    Finalizando detalhes no App. App pronto.

--- app/src/main/java/me/dio/soccernews/ui/MainActivity.java
+++ app/src/main/java/me/dio/soccernews/ui/MainActivity.java
@@ -1,8 +1,6 @@
-package me.dio.soccernews;
+package me.dio.soccernews.ui;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,30 +8,22 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import me.dio.soccernews.R;
 import me.dio.soccernews.databinding.ActivityMainBinding;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        me.dio.soccernews.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard)
-                .build();
+        // Passing each menu ID as a set of Ids because each  menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,61 +1,70 @@
 package me.dio.soccernews.ui.news;
 
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import me.dio.soccernews.data.remote.SoccerNewsApi;
+import me.dio.soccernews.data.SoccerNewsRepository;
 import me.dio.soccernews.domain.News;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends ViewModel {
 
+    public enum State {
+        DOING, DONE, ERROR
+    }
+
     //a intenção foi criar um LiveData onde retornasse a lista de notícias
     private final MutableLiveData<List<News>> news = new MutableLiveData<>();
-    private final SoccerNewsApi api;
+    private final MutableLiveData<State> state = new MutableLiveData<>();
 
     public NewsViewModel() {
-
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl("https://liviavilarinho.github.io/soccer-news-api/")
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        api = retrofit.create(SoccerNewsApi.class);
-
         //metodo findNews tem a chamada http do retrofit
-        this.findNews();
+            this.findNews();
+        }
 
-    }
 //quando viewModel for construído (acima), ele vai fazer a chamada automaticamente
     //dando tudo certo, ele estimula o LiveData que é o responsável por "avisar" a nossa tela que a lista
     //esta pronta para ser exibida
 
 
-    private void findNews() {
-        api.getNews().enqueue(new Callback<List<News>>() {
+    public void findNews() {
+        state.setValue(State.DOING);
+        SoccerNewsRepository.getInstance().getRemoteApi().getNews().enqueue(new Callback<List<News>>() {
             @Override
-            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
-                if(response.isSuccessful()) {
+            public void onResponse(@NonNull Call<List<News>> call, @NonNull Response<List<News>> response) {
+                if (response.isSuccessful()) {
                     news.setValue(response.body());
+                    state.setValue(State.DONE);
                 } else {
-                    //  TODO pensar em uma estratégia de tratamento de erros
+                    state.setValue(State.ERROR);
                 }
             }
 
             @Override
-            public void onFailure(Call<List<News>> call, Throwable t) {
+            public void onFailure(@NonNull Call<List<News>> call, @NonNull Throwable t) {
                 //  TODO pensar em uma estratégia de tratamento de erros
 
             }
         });
     }
 
-    public LiveData<List<News>> getNews() { return this.news; }
+    public void saveNews(News news) {
+        AsyncTask.execute(() -> SoccerNewsRepository.getInstance().getLocalDb().newsDao().save(news));
+    }
+
+    public LiveData<List<News>> getNews() {
+        return this.news;
+    }
+
+    public LiveData<State> getState() {
+        return this.state;
+    }
 }
